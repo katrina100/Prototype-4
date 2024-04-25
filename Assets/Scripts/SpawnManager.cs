ee25a3da4fe3e31cbe5186076c60d458a2c6258a
katrina <katrinaalejo105@gmail.com>
2024-04-25 06:01:00 -0600

    Added Increase powerup & more enemy

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,18 +6,40 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
     private float spawnRange = 9.0f;
+    public int enemyCount;
+    public GameObject powerupPrefab;
+    public int waveNumber = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate( enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation); 
+        SpawnEnemyWave(waveNumber);
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+
+        if (enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber); 
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation); 
+
+        }
     }
 
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate( enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation); 
+        }
+
+    }
+    
     private Vector3 GenerateSpawnPosition()
     {
         float spawnX = Random.Range(-spawnRange, spawnRange);
