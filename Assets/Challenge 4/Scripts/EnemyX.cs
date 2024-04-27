d3981b5081fef84087f25504d37e03c8054b47ea
katrina <katrinaalejo105@gmail.com>
2024-04-27 12:03:52 -0600

    Finished Challenge 4

---
+++ Assets/Challenge 4/Scripts/EnemyX.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyX : MonoBehaviour
+{
+    private float speed = 10.0f;
+    private Rigidbody enemyRb;
+    public GameObject playerGoal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        playerGoal = GameObject.Find("Player Goal");
+    }
+
+    void Update()
+    {
+        // Calculate the direction towards the player goal
+        Vector3 lookDirection = (playerGoal.transform.position - transform.position);
+
+        // Apply a force in that direction
+        enemyRb.AddForce(lookDirection * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // If enemy collides with either goal, destroy it
+        if (other.gameObject.name == "Enemy Goal")
+        {
+            Destroy(gameObject);
+        } 
+        else if (other.gameObject.name == "Player Goal")
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+}
